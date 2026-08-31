ecd6699141268a2beb4d4029d6a4c8f9732571be
Eamonn Flynn <eamonnf@msn.com>
2026-08-31 22:42:10 +0100

    feat: Angular upgrade and CSS updates -  Upgraded to v22 of angular - added Vite test coverage - Updated Tailwind to verision 4 - add fonts for heading body and code snippets

--- src/src/app/app.ts
+++ src/src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('Learning Angular ');
+  readonly codeSnippet = `const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Tech World\\n');
+});`;
+
 }
